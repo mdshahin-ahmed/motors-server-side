7cbad92fa6eb8c079d48cd6557894c0f3a12f08d
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2021-12-04 01:17:27 +0600

    Done send data from ui

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
-
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mh2ii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
-        console.log('database connected');
+        const database = client.db("Motors");
+        const productsCollection = database.collection("products");
+        const usersCollection = database.collection('users');
+
+        app.get('/products', async(req,res)=>{
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.json(products);
+        });
+
+        app.post('/products', async(req, res)=>{
+            const product = req.body;
+            // console.log('hit api', product);            
+            const result = await productsCollection.insertOne(product);
+            console.log(result);
+            res.json(result);
+        })
+
+
+
+        app.get('/users/:email', async(req,res)=>{
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if(user?.role === 'admin'){
+                isAdmin = true;
+            }
+            res.json({admin: isAdmin});
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async(req, res) => {
+            const user = req.body;
+            const filter = {email: user.email};
+            const updateDoc = {$set: {role: 'admin'}};
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
     }
     finally {
         // await client.close();
